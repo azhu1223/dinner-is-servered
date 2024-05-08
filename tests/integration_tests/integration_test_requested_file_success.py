029134b486e92d6ee89dbe0541172344ec4cabcd
Andrew <andrewzhu1223@g.ucla.edu>
2024-05-08 06:23:59 +0000

    Added new integration tests to test requesting valid files and invalid files.
    Change-Id: I42fd9e57440453c89f5ae5fd5fa797ab7203e8a4

---
+++ tests/integration_tests/integration_test_requested_file_success.py
@@ -0,0 +1,54 @@
+#!/bin/python3
+
+import subprocess as sp
+import argparse
+
+config_folder = "../configs"
+config_name = "/static_file_serving_location"
+request_folder = "/static"
+request_file = "/oof.txt"
+expected_output_folder = "../resources/text"
+expected_output_name = "/oof.txt"
+
+def setup_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("server_path")
+
+    return parser
+
+def main():
+    args = setup_parser().parse_args()
+
+    # Start the server process.
+    server_process = sp.Popen([args.server_path, config_folder + config_name, "--disable-file-logging"], stdout=sp.PIPE)
+
+    request_path = request_folder + request_file
+
+    print(request_path)
+
+    # Send HTTP request to the server via curl.
+    # Send the output (response header and content) to a pipe.
+    curl_process = sp.Popen(["curl", "localhost:80" + request_path, "-s"], stdout=sp.PIPE)
+
+    # Compare the output of the curl to the expected output using diff.
+    diff_process = sp.Popen(["diff", "-", expected_output_folder + expected_output_name], stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE)
+
+    curl_process_output = curl_process.stdout.read()
+
+    print(curl_process_output.decode())
+
+    # Kill the server
+    server_process.kill()
+
+    # Pipe the curl process stdout to the diff process' stdin.
+    ret, err = diff_process.communicate(input=curl_process_output)
+
+    # Check that the return code of diff is 0 (i.e. the output from curl is the expected output).
+    if diff_process.returncode == 0:
+        return 0
+    else:
+        return 1
+
+if __name__ == "__main__":
+    return_code = main()
+    exit(return_code)
