b8e4580a2b0416413c321e34c96b8197a2083fb9
Devel User <akivabrookler@g.ucla.edu>
2024-05-29 21:45:53 +0000

    Created landing page, configured to /app
    Change-Id: Ie1d554e8786f3ddac05a273c5d77e9b44bbfa4c8

--- src/app_handler.cc
+++ src/app_handler.cc
@@ -1,5 +1,7 @@
 #include "app_handler.h"
 #include "logging_buffer.h"
+#include <fstream>
+
 
 
 AppHandler::AppHandler(LoggingBuffer* logging_buffer) : RequestHandler(), logging_buffer_(logging_buffer) {}
@@ -11,12 +13,39 @@ http::response<http::vector_body<char>> AppHandler::handle_request(const http::r
 
 
 http::response<http::vector_body<char>> AppHandler::generate_landing_page(){
-    std::vector<char> response_body_vector;
-    
-    auto response = http::response<http::vector_body<char>>(http::status::ok, 11U, response_body_vector);
 
-    response.set(http::field::content_type, "text/plain");
-    response.set(http::field::content_length, std::to_string(response_body_vector.size()));
+    http::response<http::vector_body<char>> response;
+
+    std::string file_path = "../app_pages/index.html";
+
+    std::ifstream file(file_path, std::ios::in | std::ios::binary);
+
+    if (!file.is_open()) {
+        logging_buffer_->addToBuffer(ERROR, "File not found");
+        response = http::response<http::vector_body<char>>(http::status::not_found, 11U);
+        response.prepare_payload();
+        return response;
+    }
+
+    // Get file size
+    file.seekg(0, std::ios::end);
+    std::streampos file_size = file.tellg();
+    file.seekg(0, std::ios::beg);
+    logging_buffer_->addToBuffer(INFO, "Obtained file size of " + std::to_string(file_size));
+
+    // Allocate buffer for file
+    std::vector<char> res(file_size);
+
+    // Read file content into the response buffer after the header
+    if (!file.read(res.data(), file_size)) {
+        // Failed to read file
+        file.close();
+        logging_buffer_->addToBuffer(ERROR, "Failed to read file");
+    }
+
+    response = http::response<http::vector_body<char>>(http::status::ok, 11U, res);
+    response.set(http::field::content_type, "text/html");
+    response.set(http::field::content_length, std::to_string(res.size()));
 
     response.prepare_payload(); 
 
